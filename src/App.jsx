536fb82a16a87207098fb88e82094cc0d77121aa
troy8chen <troy8chen@gmail.com>
2025-01-07 13:46:49 -0800

    Initial Commit: tailwind css setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-violet-300 font-bold"> Welcome !</h1>
+    </div>
+  )
+}
+
+export default App
